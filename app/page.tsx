ddf0498936d1cfccda14274681861eb6f73b8b0b
Alexandre Dias <alexandredias027@gmail.com>
2024-11-07 00:15:40 -0300

    adicionado projeto no repositório

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./_components/ui/button";
+
+const Home = () => {
+  return (
+    <Button>Olá mundo</Button>
+  );
+}
+
+export default Home;
---
+++ app/subscription/page.tsx
@@ -0,0 +1,5 @@
+const SubscripitonPage = () => {
+    return (<h1>subscripiton page</h1>);
+}
+
+export default SubscripitonPage;
